4f9c714e14dda166558330632820e2e57bd7c310
Amol Wankhede <amolsw.nz@gmail.com>
2025-02-18 12:37:02 +1300

    initial commit

---
+++ app/src/Page.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace {
+
+    use SilverStripe\Security\Permission;
+    use SilverStripe\Security\Security;
+
+    use SilverStripe\CMS\Model\SiteTree;
+    use SilverStripe\Forms\FieldList;
+    use SilverStripe\Security\Member;
+
+    class Page extends SiteTree
+    {
+        private static $db = [];
+
+        private static $has_one = [];
+
+        
+    }
+}

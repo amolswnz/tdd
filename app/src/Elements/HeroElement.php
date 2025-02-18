8f6de51150656a4ab7e2de177b641cac5c4d459c
Amol Wankhede <amolsw.nz@gmail.com>
2025-02-18 15:37:55 +1300

    Add hero block

---
+++ app/src/Elements/HeroElement.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Elements;
+
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\CompositeValidator;
+use DNADesign\Elemental\Models\BaseElement;
+
+class HeroElement extends BaseElement
+{
+    private static string $singular_name = 'hero element';
+    
+    private static string $plural_name = 'hero elements';
+    
+    private static string $description = 'Hero element';
+    
+    private static bool $inline_editable = false;
+
+    public function getCMSFields(): FieldList
+    {
+        $fields = parent::getCMSFields();
+
+        return $fields;
+    }
+
+    public function getCMSCompositeValidator(): CompositeValidator
+    {
+        $validator = parent::getCMSCompositeValidator();
+
+        return $validator;
+    }
+
+    public function getSummary(): string
+    {
+        return 'This is page hero block element';
+    }
+
+    public function getType(): string
+    {
+        return 'Hero block element';
+    }
+}

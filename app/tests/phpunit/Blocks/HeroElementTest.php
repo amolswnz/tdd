fbdffe0b0f6905af6ed50891751e6c7b384929f5
Amol Wankhede <amolsw.nz@gmail.com>
2025-02-20 13:17:02 +1300

    Add Silverstripe linkfield module and add check for fields as per design

--- app/tests/phpunit/Blocks/HeroElementTest.php
+++ app/tests/phpunit/Blocks/HeroElementTest.php
@@ -4,7 +4,9 @@ namespace Tests\Blocks;
 
 use App\Elements\HeroElement;
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Forms\TextField;
 use SilverStripe\Dev\SapphireTest;
+use SilverStripe\LinkField\Form\LinkField;
 
 class HeroElementTest extends SapphireTest
 {
@@ -21,5 +23,24 @@ class HeroElementTest extends SapphireTest
         $block = singleton(HeroElement::class);
 
         $this->assertEquals('Hero block element', $block->getType());
+    }
+
+    public function test_cms_fields(): void
+    {
+        $obj = singleton(HeroElement::class);
+        $fields = $obj->getCMSFields();
+
+        $this->assertInstanceOf(HeroElement::class, $obj);
+
+        $expectedInstances = [
+            'Heading' => TextField::class,
+            'Content' => TextField::class,
+            'CTALink' => LinkField::class,
+        ];
+
+        foreach ($expectedInstances as $key => $class) {
+            $msg = sprintf('The field %s should be an instance of %s::class', $key, $class);
+            $this->assertInstanceOf($class, $fields->dataFieldByName($key), $msg);
+        }
     }
 }

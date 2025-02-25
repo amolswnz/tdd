d757dd4edc97101f191c446d4d20f79715f65fe0
Amol Wankhede <amolsw.nz@gmail.com>
2025-02-25 16:48:19 +1300

    Add required fields

--- app/src/Elements/HeroElement.php
+++ app/src/Elements/HeroElement.php
@@ -3,6 +3,7 @@
 namespace App\Elements;
 
 use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\RequiredFields;
 use SilverStripe\LinkField\Models\Link;
 use SilverStripe\Forms\CompositeValidator;
 use SilverStripe\LinkField\Form\LinkField;
@@ -41,6 +42,11 @@ class HeroElement extends BaseElement
     public function getCMSCompositeValidator(): CompositeValidator
     {
         $validator = parent::getCMSCompositeValidator();
+
+        $validator->addValidator(RequiredFields::create([
+            'Heading',
+            'Content'
+        ]));
 
         return $validator;
     }
